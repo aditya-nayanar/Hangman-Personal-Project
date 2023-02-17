71bf388e361ced00e77c954650e552006f4a839f
Aditya <adityavn08@gmail.com>
2023-02-17 14:11:40 +1100

    Bug fixes: Not showing previous guesses, not correctly ending game when all letters guessed correctly

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import requests
 
+def welcome_msg():
+    ''' Welcome users to the game and check if user wants to play'''
+    while True:
+        startmsg = input("Welcome! Would you like to play Hangman? (Y/N)").lower()
+        if startmsg not in ['y', 'n']:
+            print("Please type Y to start playing or N to quit")
+            continue
+        elif startmsg in 'n':
+            print("Goodbye!")
+            quit()
+        elif startmsg in 'y':
+            break
 def generate_word():
-    # word = "https://random-word-api.herokuapp.com/word"
-    # response = requests.get(word)
-    # word = list(response.text)
-    #
-    # word.pop(0)
-    # word.pop(0)
-    # word.pop(-1)
-    # word.pop(-1)
-    #
-    # return "".join(word)
-    return "testing"
+    '''Generate random word'''
+    word = "https://random-word-api.herokuapp.com/word"
+    response = requests.get(word)
+    word = list(response.text)
+
+    word.pop(0)
+    word.pop(0)
+    word.pop(-1)
+    word.pop(-1)
+
+    return "".join(word)
 
 def play_round(word, lives):
+    '''Start a round of hangman based on a given word and number of lives'''
     print("\nDEBUGGING: Word to generated is", word)
     print("The word to guess has", len(word), "letters")
     print("Type \"#\" to quit at anytime")
@@ -38,6 +51,10 @@ def play_round(word, lives):
         print("Previous incorrect word guesses", prev_guesses_word)
         guess = str(input("Guess a letter or word: ")).lower().strip()
 
+        # Cheat code to show word to guess
+        if guess == '%':
+            print("\nWord to guess:", word)
+            continue
         # Check if user wants to quit
         if guess == '#':
             quit()
@@ -45,17 +62,23 @@ def play_round(word, lives):
         elif not len(guess) == 1 and not len(guess) == len(word):
             print("Your guess is not a letter or a word the same length, try again!")
             continue
-        # Check if letter or word has been guessed already
-        elif guess in prev_guesses_letter or prev_guesses_word:
-            print("You have already guessed this letter or word, try again!")
-            continue
         # Check if the guess is a word and matches word
         if len(guess) > 1:
+            # Check if word has been guessed already
+            if guess in prev_guesses_word:
+                print("You have already guessed this word, try again!")
+                continue
+            # Check if correct word
             if guess in word:
-                print("Well done! The word was:", word)
+                print("\nWell done! The word was:", word)
                 break
-            print("You guess is incorrect")
+            print("Your guess is incorrect")
+            prev_guesses_word.append(guess)
             lives -= 1
+            continue
+        # If not word, check if letter has been guessed already
+        if guess in prev_guesses_letter:
+            print("You have already guessed this word, try again!")
             continue
         # Check if letter is in the word
         if guess in word:
@@ -63,35 +86,31 @@ def play_round(word, lives):
                 pass
                 if guess in word[i]:
                     word_list[i] = guess
+            # If only one letter remaining to guess, check if word is complete
+            if '_' not in word_list:
+                print("\nWell done! The word was:", word)
+                break
             continue
         print("\'", guess, "\'", "is not not in the word")
+        prev_guesses_letter.append(guess)
         lives -= 1
 
 
 def main():
 
-    # Welcome message and start menu for players
-    # while True:
-    #     startmsg = input("Welcome! Would you like to play Hangman? (Y/N)").lower()
-    #     if startmsg not in ['y', 'n']:
-    #         print("Please type Y to start playing or N to quit")
-    #         continue
-    #     elif startmsg in 'n':
-    #         print("Goodbye!")
-    #         quit()
-    #     elif startmsg in 'y':
-    #         print("The game has started")
-    #         break
+    # Welcome user
+    welcome_msg()
 
-    # Generate word for player to guess
-    word = generate_word()
+    # Set lives
+    lives = 8
 
-    # Play a round
-    play_round(word, 2)
-
-
-
+    # Start playing
     while True:
+        # Start one round
+        print("Preparing word...")
+        play_round(generate_word(), lives)
+
+        # Once completed, check if user wants to keep playing
         retry = str(input("Would you like to play again? (Y/N)")).lower()
         if retry not in ['y', 'n']:
             print("Please type Y to retry or N to quit")
@@ -100,14 +119,7 @@ def main():
             print("Goodbye!")
             quit()
         elif retry in 'y':
-            break
-
-    play_round(generate_word(), 2)
-
-
-
-
-
+            continue
 
 if __name__ == '__main__':
     main()
